9832ebbfd4201753bed3cca5da9308d25b13df82
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2025-04-15 02:08:22 -0300

    Two sum solution in Java using O(n^2) time complexity

---
+++ solutions/src/main/java/com/bruno/santana/two_sum/Solution.java
@@ -0,0 +1,27 @@
+package com.bruno.santana.two_sum;
+
+// https://leetcode.com/problems/two-sum/
+
+// algorithm with O(n2) time complexity
+
+class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        int [] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                int sum = nums[i] + nums[j];
+                if (sum == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+    
+}
