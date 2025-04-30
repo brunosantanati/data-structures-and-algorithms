785e81e6c100cf9f2968031cac6e9faa37660f29
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2025-04-30 02:44:53 -0300

    Add a 2nd solution for Valid Parentheses in Java

---
+++ solutions-java/src/main/java/com/bruno/santana/valid_parentheses/Solution2.java
@@ -0,0 +1,22 @@
+package com.bruno.santana.valid_parentheses;
+
+import java.util.Stack;
+
+public class Solution2 {
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+
+}
---
+++ solutions-java/src/test/java/com/bruno/santana/valid_parentheses/Solution2Test.java
@@ -0,0 +1,57 @@
+package com.bruno.santana.valid_parentheses;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Solution2Test {
+
+    Solution2 solution = new Solution2();
+
+    @Test
+    void testScenario1() {
+        boolean result = solution.isValid("()");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void testScenario2() {
+        boolean result = solution.isValid("()[]{}");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void testScenario3() {
+        boolean result = solution.isValid("(]");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void testScenario4() {
+        boolean result = solution.isValid("([])");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void testScenario5() {
+        boolean result = solution.isValid("[");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void testScenario6() {
+        boolean result = solution.isValid("((");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void testScenario7() {
+        boolean result = solution.isValid("){");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void testScenario8() {
+        boolean result = solution.isValid("({[]}))");
+        Assertions.assertFalse(result);
+    }
+}
